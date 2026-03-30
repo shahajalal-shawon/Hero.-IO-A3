56a8651485efcd15b6726b42cf60467261b9b120
Shahajalal-shawon <shahajalalsheikh001@gmail.com>
2026-03-30 13:40:02 -0700

    added the footer

--- src/Pages/Errorpage/Errorpage.jsx
+++ src/Pages/Errorpage/Errorpage.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Errorpage = () => {
   return (
-    <div>Errorpage</div>
+    <div className="flex flex-col justify-center items-center">
+            <h1 className="text-4xl">Opps!!!!</h1>
+            <p>Page Not Found...</p>
+            {/* <button onClick={() => setHome(-1)}>Go Home</button> */}
+        </div>
   )
 }
